3323b9efb3d29c2e00b9afa91d14f833a4aed8b5
limo <xiaoho90@163.com>
2014-10-15 10:02:59 +0800

    3

--- src/tutorial/begin.java
+++ src/tutorial/begin.java
@@ -1,13 +1,11 @@
 package tutorial;
 
-import com.opensymphony.xwork2.ActionSupport;  
+import com.opensymphony.xwork2.ActionSupport;
 public class begin extends ActionSupport{
 
 	/**
-	 * 
+	 *
 	 */
-	private static final long serialVersionUID = 1L;
-	public String execute(){
     	return SUCCESS;
     }
 }
